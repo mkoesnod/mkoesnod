66bbfe46c5cd3aba20b0ecb43e44b67240bde19f
Marie Koesnodihardjo <mkoesnod@gmail.com>
2016-08-15 15:08:18 -0700

    added json data

---
+++ resume.js
@@ -0,0 +1,10 @@
+{ "contactInfo": {
+   "name": "Marie Koesnodihardjo",
+   "contacts": {
+     "mobile": "(650) 421-5043",
+     "email": "mkoesnod@gmail.com",
+     "github": "github.com/mkoesnod",
+     "linkedin": "linkedin.com/marie-koesnodihardjo"
+  }
+}
+}
